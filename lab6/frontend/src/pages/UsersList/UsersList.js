0d4f0dcb42a2486424fa9faa6c6bddf7e46271a3
mikhail.gasin <mikhail.gasin@fors.ru>
2022-10-31 17:49:38 +0300

    Added add user functionality

--- lab6/frontend/src/pages/UsersList/UsersList.js
+++ lab6/frontend/src/pages/UsersList/UsersList.js
@@ -1,4 +1,5 @@
 import React, {  useState, useEffect } from "react";
+import {  Link  } from "react-router-dom"
 
 import UserTable from "../../components/tables/UserTable/UserTable";
 
@@ -6,6 +7,8 @@ export default function UsersList() {
 
     return (
         <div className="container">
+            <h1 className="mt-3 d-inline">User list</h1>
+            <Link to="/add" className="btn btn-success d-inline mx-5">Add new</Link>
             <UserTable/>
         </div>
     )
