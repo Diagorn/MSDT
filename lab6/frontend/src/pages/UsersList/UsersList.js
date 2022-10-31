cbdf999cd5604f412c02e57d1e72f5299d7e6830
mikhail.gasin <mikhail.gasin@fors.ru>
2022-10-31 17:51:15 +0300

    Frontend code cleanup

--- lab6/frontend/src/pages/UsersList/UsersList.js
+++ lab6/frontend/src/pages/UsersList/UsersList.js
@@ -1,4 +1,4 @@
-import React, {  useState, useEffect } from "react";
+import React from "react";
 import {  Link  } from "react-router-dom"
 
 import UserTable from "../../components/tables/UserTable/UserTable";
